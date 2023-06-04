1ace856098014d0fb98ffe51cfd03a2d1b4b36f2
Abyaz1234 <124512813+Abyaz1234@users.noreply.github.com>
2023-06-04 11:21:11 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+function bedroom(){
+  window.location="bedroom.html";
+}
+
+function bottles(){
+  window.location="bottles.html";
+}
+
+function tvandac(){
+  window.location="tvandac.html";
+}
+
+function desk(){
+  window.location="desk.html";
+}
+
+function fruitbasket(){
+  window.location="fruitbasket.html";
+}
